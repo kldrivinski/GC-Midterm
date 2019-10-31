5a46742efef651f6efa2235f7a309b889bb0d081
Laura Xu <lauraxu13@gmail.com>
2019-10-31 11:52:14 -0400

    skeleton of js, html done, wokring on css

---
+++ script.js
@@ -0,0 +1,45 @@
+// On click of start Button:
+//     allows cards to start fliping over on click
+//     starts timer
+
+
+
+// Assing random matching IDs 
+//     same ID is used twice for matching
+//     once fipped over  and matched card needs to disappear
+//     once clicked display ID "color" extra is image
+
+
+// Animation
+//     flipping
+//     disappearing
+
+// Eventlinisiter
+//     use array once array is empty then 
+//     all cards are gone text appears "you won"
+    
+// If statement 
+//     timer needs to stop when cards are gone
+
+// Restart refresh page
+
+
+
+// new Page 
+// Press start  --- Eventlistener
+// Triggers clicking of cards
+// Triggers timer -- 
+
+// Array  of cards with IDs, 2 Ids need to match
+// For cards of cards 
+// IF ID match then funtion car disappears
+// else cards flip back and 
+
+// Event addEventListener
+// when all cards are done text appears "you won"
+
+// Reset --- refresh
+
+
+
+location.reload();
